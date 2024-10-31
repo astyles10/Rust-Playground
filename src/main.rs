bb4d3ef91fdd09c777e85c65b6fd8f3f1b1ac857
astyles10 <alexanderstyles10@gmail.com>
2024-10-31 16:59:37 +1100

    loops

--- src/main.rs
+++ src/main.rs
@@ -17,46 +17,82 @@ fn fail (error: &String) {
   exit(1);
 }
 
-fn remove_whitespace_from_string(input: &str) -> String {
+fn clean_string(input: &str) -> String {
   let mut tmp = input.replace(" ", "");
   tmp = tmp.replace("\t", "");
+  tmp = tmp.replace("\n", "");
   tmp
 }
 
-fn get_first_number_from_string(input: &str) {
-  let mut start_index: Option<usize> = None;
-  let mut end_index: usize = 0;
-  for c in input.char_indices() {
-    if c.1.is_numeric() && start_index.is_none() {
-      start_index = Some(c.0);
-    } else if !c.1.is_numeric() && start_index.is_some() {
-      end_index = c.0;
+fn do_calculate(input: &str, start_index: usize, operator: char) {
+  println!("Start do_calculate: start_index = {}, input.len() = {}, input = '{}'", start_index, input.len(), input);
+  println!("Current operator: {}", operator);
+  if start_index >= input.len() {
+    return;
+  }
+
+  let substr: &str = &input[start_index..];
+  println!("starting substr = {}", substr);
+
+  // Need to advance the iterator to the index in a rust way
+  // https://stackoverflow.com/questions/62186871/how-to-correctly-use-peek-in-rust
+  for c in substr.char_indices() {
+    if c.1.is_numeric() {
+      let substr: &str = &input[c.0..];
+      println!("inside loop: current substr = {}", substr);
+      let num: (usize, i64) = parse_number_string(substr);
+      let end_index: usize = num.0;
+      // println!("Got index: {}, number: {}, input.len() {}", num.0, num.1, input.len());
+      println!("Got number: {}", num.1);
+      do_calculate(substr, end_index, operator);
+      break;
+    }
+    else if !c.1.is_numeric() && validate_operator(c.1) {
+      do_calculate(input, c.0, c.1);
       break;
     }
   }
-
-  match start_index {
-    None => {
-      fail(&String::from("Could not find number"));
-    },
-    Some(start_index) => {
-      println!("Got start index: {}", start_index);
-      println!("Got end index: {}", end_index);
-      let substr : &str = &input[start_index..end_index];
-      println!("Got string slice: {}", substr);
-    }
-  };
-  // Can get non-mutable value by dereferencing as seen here:
-  // let start_index: usize = *start_index.get_or_insert(0);
 }
 
-fn get_operator_from_string(input: &str) {
-  for c in input.chars() {
-    // Check if + - * / ^ % !
+// Return the number and the end index
+fn parse_number_string(input: &str) -> (usize, i64) {
+  let mut end_index: usize = 0;
+  for c in input.char_indices() {
+    if !c.1.is_numeric() {
+      end_index = c.0;
+      break;
+    } else if c.0 == input.len() - 1 {
+      end_index = c.0 + 1;
+    }
+  }
+  if end_index == 0 {
+    return (input.len(), 0)
+  }
+
+  let substr: &str = &input[..end_index];
+  let number: Result<i64, std::num::ParseIntError> = substr.parse::<i64>();
+  println!("parse_number_string: substr = {} end_index = {}", substr, end_index);
+  match number {
+    Ok(num) => {
+      (end_index, num)
+    },
+    Err(err) => {
+      fail(&format!("parse_number_string: {}", err));
+      (0, 0)
+    }
   }
 }
 
+fn validate_operator(input: char) -> bool {
+  let operators = "+-*/%^()";
+  if operators.contains(input) {
+    return true
+  }
+  fail(&format!("invalid operator received: {}", input));
+  false
+}
+
 fn echo_user_input(user_input_buffer: &str) {
-  let cleaned_string = remove_whitespace_from_string(user_input_buffer);
-  get_first_number_from_string(&cleaned_string);
+  let cleaned_string = clean_string(user_input_buffer);
+  do_calculate(&cleaned_string, 0, 'n');
 }
